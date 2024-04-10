ce0cdb50e6ca99946d3ea8a13719b92500ede33d
Adam Charron <146114816+acharron-hl@users.noreply.github.com>
2024-04-10 17:18:24 -0400

    Add hostname replacement support and additional options for long runners (#5)
    * Increase long runner timeout
    
    * Add host replacement support for orch sites and option support for callback payloads
    
    * Fix psalm

--- src/Orch/OrchSite.php
+++ src/Orch/OrchSite.php
@@ -6,7 +6,10 @@
 
 namespace Garden\Sites\Orch;
 
-use Garden\Http\HttpHandlerInterface;
+use Garden\Http\HttpRequest;
+use Garden\Http\HttpResponse;
+use Garden\Sites\Clients\SiteHttpClient;
+use Garden\Sites\Cluster;
 use Garden\Sites\Site;
 use Garden\Sites\SiteRecord;
 use Garden\Utils\ArrayUtils;
@@ -29,6 +32,33 @@ class OrchSite extends Site
     }
 
     /**
+     * Add additional default cookies to requests.
+     *
+     * @return SiteHttpClient
+     */
+    public function httpClient(): SiteHttpClient
+    {
+        $httpClient = parent::httpClient();
+        $realHostname = $this->getRealHostName();
+        $clusterRouterHostname = $this->getClusterRouterHostname();
+        if ($clusterRouterHostname) {
+            $kludgedBaseUrl = $this->replaceHostnameInUrl($this->getBaseUrl());
+            $httpClient->setBaseUrl($kludgedBaseUrl);
+            $httpClient->setDefaultHeader("Host", $realHostname);
+
+            $httpClient->addMiddleware(function (HttpRequest $request, callable $next) use (
+                $realHostname
+            ): HttpResponse {
+                $request->setUrl($this->replaceHostnameInUrl($request->getUrl()));
+                $request->setHeader("Host", $realHostname);
+                return $next($request);
+            });
+        }
+
+        return $httpClient;
+    }
+
+    /**
      * @inheritDoc
      */
     protected function loadSiteConfig(): array
@@ -41,5 +71,53 @@ class OrchSite extends Site
             return $b;
         });
         return $mergedConfig;
+    }
+
+    /**
+     * Given a url, try to replace it's base url so it routes with the cluster router.
+     *
+     * @param string $url
+     * @return string
+     */
+    public function replaceHostnameInUrl(string $url): string
+    {
+        $clusterRouterHostname = $this->getClusterRouterHostname();
+        if ($clusterRouterHostname === null) {
+            return $url;
+        }
+
+        $realHostname = $this->getRealHostName();
+        $kludgedUrl = str_replace($realHostname, $clusterRouterHostname, $url);
+        $kludgedUrl = str_replace("https", "http", $kludgedUrl);
+        return $kludgedUrl;
+    }
+
+    /**
+     * @return string
+     */
+    private function getRealHostName(): string
+    {
+        $baseUrl = $this->getBaseUrl();
+        $realHostname = parse_url($baseUrl, PHP_URL_HOST);
+        return $realHostname;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getClusterRouterHostname(): ?string
+    {
+        switch ($this->getCluster()->getRegionID()) {
+            case Cluster::REGION_AMS1_PROD1:
+                return "haproxy-router.ams1-routing-prod1.vanilladev.com";
+            case Cluster::REGION_SJC1_PROD1:
+                return "haproxy-router.sjc1-routing-prod1.vanilladev.com";
+            case Cluster::REGION_YUL1_PROD1:
+                return "haproxy-router.yul1-routing-prod1.vanilladev.com";
+            case Cluster::REGION_YUL1_DEV1:
+                return "haproxy-router.yul1-routing-dev1.vanilladev.com";
+        }
+
+        return null;
     }
 }
